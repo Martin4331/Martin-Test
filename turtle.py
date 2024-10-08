e967a7fbe193cf0110d9671c0aa4c85e70f698f8
Martin4331 <sorasdekkan12@gmail.com>
2024-10-09 00:26:24 +0200

    Create turtle.py
    La til turtle.py fil

---
+++ turtle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 17:19:28 2024
+
+@author: soras
+"""
+
+from turtle import *
+
+for f in range(4):
+    forward(100)
+    left(360/4)
+   
+turtle.done()
